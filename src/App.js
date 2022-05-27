a86af1271896fb115f6a22b7fda3abdf0c432e74
Manuel Gil <manuelgilmoltrasio@gmail.com>
2022-05-27 16:33:19 -0300

    multiple symbols bug solved. multiples '.' bug solved. After evaluating an operation, if number is entered, output resets

--- src/App.js
+++ src/App.js
@@ -3,27 +3,49 @@ import Screen from './components/Screen';
 import Button from './components/Button';
 import { useState } from 'react';
 import { evaluate } from 'mathjs';
+import { RiDeleteBack2Line } from 'react-icons/ri'
 
 function App() {
 
     const [input, setInput] = useState('');
+    const [evaluated, setEvaluated] = useState('false');
+    const symbols = ['+','-','*','/','.'];
 
     const addInput = (value) => {
+        if (input) {
+            if (symbols.includes(value)) {
+                let lastChar = input.slice(input.length-1, input.length);
+                if (symbols.includes(lastChar)) {
+                    const newInput = input.slice(0, input.length-1) + value;
+                    setInput(newInput);
+                    setEvaluated('false');
+                    return
+                }
+            } else if(evaluated === 'true') {
+                setInput(value);
+                setEvaluated('false');
+                return 
+            }
+        }
+        setEvaluated('false');
         setInput(input + value);
     }
 
     const handleClear = () => {
         setInput('');
+        setEvaluated('false');
     }
 
     const handleCharDelete = () => {
         const newOutput = input.slice(0, input.length-1)
         setInput(newOutput);
+        setEvaluated('false');
     }
 
     const calculate = () => {
-        if (input) {
-            setInput(evaluate(input));
+        if (input && input.replace('.', '')) {
+            setInput(String(evaluate(input)));
+            setEvaluated('true');
         }
     }
 
@@ -33,32 +55,32 @@ function App() {
                 <Screen outputText={input} />
                 <div className='fila-calculadora'>
                     <Button handleClick={handleClear} children="Clear" double='true'/>
-                    <Button handleClick={handleCharDelete} children="B" />
-                    <Button handleClick={addInput} children="*" />
+                    <Button handleClick={handleCharDelete} children={<RiDeleteBack2Line />} />
+                    <Button handleClick={addInput} children='*' />
                 </div>
                 <div className='fila-calculadora'>
                     <Button handleClick={addInput} children="7" />
                     <Button handleClick={addInput} children="8" />
                     <Button handleClick={addInput} children="9" />
-                    <Button handleClick={addInput} children="/" />
+                    <Button handleClick={addInput} children='/' />
                 </div>
                 <div className='fila-calculadora'>
                     <Button handleClick={addInput} children="4" />
                     <Button handleClick={addInput} children="5" />
                     <Button handleClick={addInput} children="6" />
-                    <Button handleClick={addInput} children="-" />
+                    <Button handleClick={addInput} children='-' />
                 </div>
                 <div className='fila-calculadora'>
                     <Button handleClick={addInput} children="1" />
                     <Button handleClick={addInput} children="2" />
                     <Button handleClick={addInput} children="3" />
-                    <Button handleClick={addInput} children="+" />
+                    <Button handleClick={addInput} children='+' />
                 </div>
                 <div className='fila-calculadora'>
                     <Button handleClick={addInput} children="0" />
                     <Button handleClick={addInput} children="00" />
                     <Button handleClick={addInput} children="." />
-                    <Button handleClick={calculate} children="=" />
+                    <Button handleClick={calculate} children='=' />
                 </div>
             </div>
         </div>
